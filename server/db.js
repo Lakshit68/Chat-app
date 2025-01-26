9633c60ba2cb14621d9890ebe6c41267a4ff0aee
lakshit68 <lakshitsachdeva25@gmail.com>
2025-01-26 19:36:29 +0530

    update db.js

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 function Connection() {
     const mongoURI = "mongodb://127.0.0.1:27017/chat"
@@ -7,14 +6,4 @@ function Connection() {
     .catch(err => console.log(err))
 }
 
-=======
-const mongoose = require('mongoose')
-function Connection() {
-    const mongoURI = "mongodb://127.0.0.1:27017/chat"
-    mongoose.connect(mongoURI)
-    .then(() => console.log("connected"))
-    .catch(err => console.log(err))
-}
-
->>>>>>> c372f4b5802252bf8c0e83b9e9f9893c47327896
 module.exports = Connection
